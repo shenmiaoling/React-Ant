4ee99eae774275005d9069b98d2aab02d5846fc1
shenmiaoling <913081564@qq.com>
2018-06-19 15:29:33 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const {pathExistsSync} = require('fs-extra')
+
+const server = express()
+
+nunjucks.configure('.',{
+  autoescape: true,
+  express: server
+})
+
+const locals = require('./package.json')
+
+if (pathExistsSync('./assets/webpack-assets.json')) {
+  Object.assign(locals,{
+    names: '加载中...'
+  },require('./assets/webpack-assets'))
+}
+
+server.use('/assets',express.static('assets'))
+
+server.get('*',(request,response) => {
+  response.render('index.njk', Object.assign({},locals))
+})
+
+server.listen(1234,()=>{
+  console.log('* Listening on http://localhost:1234');
+  console.log('* Use Ctrl-C to stop');
+})
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const path = require('path')
+const webpack = require('webpack')
+const autoprefixer = require('autoprefixer')
+const fromPairs = require('lodash/fromPairs')
+const AssetsPlugin = require('assets-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const dev = Boolean(process.env.WEBPACK_SERVE)
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+function getEnv() {
+  return process.env.NODE_ENV || 'development'
+}
+
+function ifDev(resolve,reject) {
+  return getEnv() == 'development' ?
+  resolve : reject
+}
+
+module.exports = {
+  entry: {
+    index: '.',
+    packages: [
+      'react',
+      'react-dom',
+      'react-router',
+      'antd'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'assets'),
+    filename: ifDev('application.js','application-[hash].js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  autoprefixer({
+                    browsers: ['last 2 versions']
+                  })
+                ]
+              }
+            },
+            {
+              loader: 'stylus-loader'
+            }
+          ]
+        })
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe?g|png|svg|gif)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(.woff2?|eot|ttf)$/,
+        loader: 'url-loader?limit=1024'
+      }
+    ]
+  },
+  plugins: [
+    new AssetsPlugin({
+      path: path.join(__dirname,'assets')
+    }),
+    new ExtractTextPlugin({
+      filename: ifDev('application.css','application-[hash].css')
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'packages',
+      filename: ifDev('packages.js','packages-[hash].js')
+    })
+  ].concat(ifDev([],[new webpack.optimize.UglifyJsPlugin()])),
+  resolve: {
+    extensions: ['.js','.jsx','.css','.styl','.json']
+  }
+}
